72d030c5da738f054024907321b90e326191a3af
Sagar Gavhane <33308798+sagar-gavhane@users.noreply.github.com>
2020-10-19 14:53:11 +0530

    add extra space main element

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout(props) {
   return (
     <div className="pb-16">
       <Header />
-      <main className="p-4">{props.children}</main>
+      <main className="py-4 px-6">{props.children}</main>
       <Footer />
     </div>
   )
