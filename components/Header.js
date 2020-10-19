fb49967e42d7a72bf5c13ab527e1b1a7953d2268
Sagar Gavhane <33308798+sagar-gavhane@users.noreply.github.com>
2020-10-19 12:20:33 +0530

    add link to home page

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
   return (
     <header>
       <div className="py-2 mt-2 text-center text-5xl font-extrabold leading-none tracking-tight">
-        <img src="/images/logo.png" className="inline-block w-20" />
+        <Link href="/">
+          <a>
+            <img src="/images/logo.png" className="inline-block w-20" />
+          </a>
+        </Link>
       </div>
     </header>
   )
