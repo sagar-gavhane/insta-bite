3b9d3645481936a177e31586aba7c00500c8da49
Sagar Gavhane <33308798+sagar-gavhane@users.noreply.github.com>
2020-10-20 23:11:26 +0530

    add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,49 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Order pizza, burger, or cake - insta-bite.com</title>
+          {/* primary meta tags */}
+          <meta
+            name="title"
+            content="Order pizza, burger, or cake  - insta-bite.com"
+          />
+          <meta
+            name="description"
+            content="Order Pizza online from Insta bite and Get home delivery within the next 45 minutes. Yummy pizza delivered fast & fresh. "
+          />
+          {/* open graph / facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://insta-bite.com/" />
+          <meta
+            property="og:title"
+            content="Order pizza, burger, or cake  - insta-bite.com"
+          />
+          <meta
+            property="og:description"
+            content="Order Pizza online from Insta bite and Get home delivery within the next 45 minutes. Yummy pizza delivered fast & fresh. "
+          />
+          <meta
+            property="og:image"
+            content="https://insta-bite.com/meta-og-image.jpg"
+          />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          {/* twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://insta-bite.com/" />
+          <meta
+            property="twitter:title"
+            content="Order pizza, burger, or cake  - insta-bite.com"
+          />
+          <meta
+            property="twitter:description"
+            content="Order Pizza online from Insta bite and Get home delivery within the next 45 minutes. Yummy pizza delivered fast & fresh. "
+          />
+          <meta
+            property="twitter:image"
+            content="https://insta-bite.com/meta-twitter-image.jpg"
           />
           <script
             dangerouslySetInnerHTML={{
