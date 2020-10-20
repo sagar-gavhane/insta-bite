1a225787d10aa36a8e0136da0552300905706cf9
Sagar Gavhane <33308798+sagar-gavhane@users.noreply.github.com>
2020-10-20 21:42:11 +0530

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 import cartService from 'services/carts'
+import * as gtag from 'utils/gtag'
+
 import 'styles/index.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   useEffect(() => {
     const cartId = localStorage.getItem('cartId')
 
