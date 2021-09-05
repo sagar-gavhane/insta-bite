e23af012637f1e3cf5734f429e3713a81a0a0c73
Sagar Gavhane <sagar.gavhane@peppercontent.in>
2021-09-05 15:26:28 +0530

    chore: remove footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import Footer from 'components/Footer'
 import Header from 'components/Header'
 
 export default function Layout(props) {
@@ -8,7 +7,6 @@ export default function Layout(props) {
     <div className="pb-16">
       <Header />
       <main className="p-4">{props.children}</main>
-      <Footer />
     </div>
   )
 }
